27e26ef2a077a81ace3004299e27e77a65312069
William Wolf <throughnothing@gmail.com>
2016-11-06 21:49:54 -0800

    Make tests a little clearer

--- test/Main.purs
+++ test/Main.purs
@@ -19,20 +19,24 @@ main = do
   -- http://www.di-mgt.com.au/sha_testvectors.html
 
   -- String
-  let hash256Abc = shaHex SHA_256 "abc"
-  assert $ hash256Abc == "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
+  assert $ do
+    let hash256Abc = shaHex SHA_256 "abc"
+    hash256Abc == "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
 
   -- Uint8Array
-  let hash512Abc = shaHex SHA_512 $ encodeUtf8 "abc"
-  assert $ hash512Abc == "ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f"
+  assert $ do
+    let hash512Abc = shaHex SHA_512 $ encodeUtf8 "abc"
+    hash512Abc == "ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f"
 
   -- DataView
-  let hash256Empty = shaHex SHA_256 $ (whole <<< fromString) ""
-  assert $ hash256Empty == "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  assert $ do
+    let hash256Empty = shaHex SHA_256 $ (whole <<< fromString) ""
+    hash256Empty == "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   -- ArrayBuffer
-  let hash512Empty = shaHex SHA_512 $ fromString ""
-  assert $ hash512Empty == "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"
+  assert $ do
+    let hash512Empty = shaHex SHA_512 $ fromString ""
+    hash512Empty == "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"
 
 
 
@@ -42,13 +46,16 @@ main = do
   let dat = "what do ya want for nothing?"
 
   -- String
-  let shaHmac256Test2 = shaHmacHex SHA_256 key dat
-  assert $ shaHmac256Test2 == "5bdcc146bf60754e6a042426089575c75a003f089d2739839dec58b964ec3843"
+  assert $ do
+    let shaHmac256Test2 = shaHmacHex SHA_256 key dat
+    shaHmac256Test2 == "5bdcc146bf60754e6a042426089575c75a003f089d2739839dec58b964ec3843"
 
   -- Uint8Array
-  let shaHmac512Test2 = shaHmacHex SHA_512 (encodeUtf8 key) (encodeUtf8 dat)
-  assert $ shaHmac512Test2 == "164b7a7bfcf819e2e395fbe73b56e0a387bd64222e831fd610270cd7ea2505549758bf75c05a994a6d034f65f8f0e6fdcaeab1a34d4a6b4b636e070a38bce737"
+  assert $ do
+    let shaHmac512Test2 = shaHmacHex SHA_512 (encodeUtf8 key) (encodeUtf8 dat)
+    shaHmac512Test2 == "164b7a7bfcf819e2e395fbe73b56e0a387bd64222e831fd610270cd7ea2505549758bf75c05a994a6d034f65f8f0e6fdcaeab1a34d4a6b4b636e070a38bce737"
 
   -- DataView
-  let shaHmac224Test2 = shaHmacHex SHA_224 ((dataView <<< encodeUtf8) key) ((dataView <<< encodeUtf8) dat)
-  assert $ shaHmac224Test2 == "a30e01098bc6dbbf45690f3a7e9e6d0f8bbea2a39e6148008fd05e44"
+  assert $ do
+    let shaHmac224Test2 = shaHmacHex SHA_224 ((dataView <<< encodeUtf8) key) ((dataView <<< encodeUtf8) dat)
+    shaHmac224Test2 == "a30e01098bc6dbbf45690f3a7e9e6d0f8bbea2a39e6148008fd05e44"
